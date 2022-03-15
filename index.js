2c6fea2b01f27b30f8366ec09d3231955ea7c96c
yugmani <yugmani@yahoo.com>
2022-03-14 22:37:59 -0700

    rewrite if into switch

--- index.js
+++ index.js
@@ -33,18 +33,34 @@ switch(x) {</br>
 
 // Tasks
 // Rewrite the "switch" into an "if"
-let browser = prompt('Enter your browser: ', '');
+// let browser = prompt('Enter your browser: ', '');
+let browser = 'Safari';
 
-  if (browser === 'Edge') {
-    console.log("You've got the Edge!");
-  } else if (
-    browser === 'Chrome' ||
-    browser === 'Firefox' ||
-    browser === 'Safari' ||
-    browser === 'Opera'
-  ) {
-    console.log('Okay we support these browsers too');
-  } else {
-    console.log('We hope that this page looks ok!');
-  }
+if (browser === 'Edge') {
+  console.log("You've got the Edge!");
+} else if (
+  browser === 'Chrome' ||
+  browser === 'Firefox' ||
+  browser === 'Safari' ||
+  browser === 'Opera'
+) {
+  console.log('Okay we support these browsers too');
+} else {
+  console.log('We hope that this page looks ok!');
+}
 
+//Rewrite "if" into "switch"
+let a = +prompt('a?', '');
+// let a = '';
+switch (a) {
+  case 0:
+    console.log(0);
+    break;
+  case 1:
+    console.log(1);
+    break;
+  case 2:
+  case 3:
+    console.log('2,3');
+  
+}
